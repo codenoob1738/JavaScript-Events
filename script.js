d44c4096f2694c4e2e2c9735493ffaad012105ef
Eton Miller <etonmiller@Etons-MacBook-Air.local>
2026-03-09 10:38:51 -0400

    Added Keyboard event

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+// Mouse Events //
+
 const button = document.getElementById("hoverButton");
 const message = document.getElementById("message");
 
-button.addEventListener("mouseover", function() {
-    message.textContent = "You are hovering over the button!";
+button.addEventListener("mouseover", function () {
+    message.textContent = "Ouch get off me!";
     this.style.backgroundColor = "hotpink";
 });
 
-button.addEventListener("mouseout", function() {
-    message.textContent = "You left the button.";
+button.addEventListener("mouseout", function () {
+    message.textContent = "Thank you";
     this.style.backgroundColor = "lightblue";
+});
+
+// Keyboard Events //
+
+const searchBar = document.getElementById("search-bar");
+const keysPressed = document.getElementById("keysPressed");
+
+document.addEventListener('keydown', function (event) {
+    keysPressed.textContent = event.key;
 });
